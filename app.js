596a3c849066782e5fcbaa0871e72998d97c33cf
CarloBerni <carlo.berni@hetic.net>
2020-03-17 20:24:48 +0100

    continue

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'pug');
+
 
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.render('index', { title: 'Qui prend quoi ?' });
+  res.render('party', { title: 'Qui prend quoi ? '});
+});
+
+app.get('/party/:id', function(req, res){
+	axios
+		.get(`${process.env.API_URL}/party/${req.params.id}`)
+		.then(({ data }) =>
+			res.render('party', {
+				party: data,
+				title: data.name,
+				url: `${process.env.FRONT_URL}:${process.env.PORT}/party/${data._id}`
+			}),
+  )
+  .catch((err) => console.log(err));
+})
+
+
+app.post('/party', function(req, res){
+  axios
+    .post(`${process.env.API_URL}/party`, req.body)
+    .then(({data}) => res.redirect(`/party/${data._id}`))
+    .catch((err) => res.send(err));
 });
 
 
