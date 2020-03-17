b921ab82129654a6455ce849712018f7ffc6a856
CarloBerni <carlo.berni@hetic.net>
2020-03-17 17:26:39 +0100

    continue

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+
+app.listen(process.env.PORT, () => 
+	console.log(`Front app listening on port ${process.env.PORT}!`),
+);
