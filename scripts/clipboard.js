0e4185c172854aedcbc1c6b737f3eeaf44f735ad
CarloBerni <carlo.berni@hetic.net>
2020-03-21 18:50:49 +0100

    add

---
+++ scripts/clipboard.js
@@ -0,0 +1,23 @@
+if (navigator.clipboard) {
+  document.querySelectorAll('[data-clipboard]').forEach(($clipboardEl) => {
+      const $button = document.createElement('button');
+      $button.innerHTML = 'Copié !';
+      setTimeout(() => ($button.innerHTML = 'Copier'), 2000);
+      $clipboardEl.parentNode.append($button);
+      $button.addEventListener(
+          'click',
+          copyToClipboard.bind(this, $clipboardEl, $button)
+      );
+  });
+} else {
+  console.warn("Pas de support")
+}
+
+function copyToClipboard($clipboardEl, $button) {
+  navigator.clipboard
+      .writeText($clipboardEl.getAttribute('data-clipboard'))
+      .then(() => {
+          console.log('Copié !');
+      })
+      .catch((err) => console.warn(err));
+}
